4a558339ad5c6c34e40fc4700f86302e71362219
Arjun Uniyal <142481196+LordMajnubhai@users.noreply.github.com>
2023-10-25 19:18:42 +0000

    comparison operators

---
+++ 5_comparison.js
@@ -0,0 +1,34 @@
+// console.log(2>0);
+// console.log(2<0);
+// console.log(2>=0);
+// console.log(2<0);
+// console.log(2==0);
+// console.log(2!=0);
+
+/*comparing two different datatypes */
+
+// console.log("2">1); //in this case js automatically converts"2" to strin
+// console.log("2"<1);
+
+// console.log(null>0)
+// console.log(null==0) //In his case it converts null to NaN
+// console.log(null>=0) //In his case it converts null to 0
+
+// console.log(undefined>0)
+// console.log(undefined==0) 
+// console.log(undefined>=0)
+/*it gives false in every case when compared to undefined */
+
+/* in js comparison and equality check orks differently */
+
+//strict check (===)
+//in this it also check datatypes while equality checks
+
+// console.log(2==="2") //it gives false
+// console.log(2=="2") //it gives true
+
+
+
+
+
+
