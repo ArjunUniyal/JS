053249424927d0af7213631e92d4f5811f4cb2a4
Arjun Uniyal <142481196+LordMajnubhai@users.noreply.github.com>
2023-10-25 17:56:07 +0000

    datatype conversion to number boolean,string

---
+++ basics/3_converson_operations.js
@@ -0,0 +1,70 @@
+//conversion to number
+
+// let score="33";
+// let score_1="65asd"
+// let score_2=null
+// let score_3=undefined
+// let score_4=false
+// let score_5="Arjun"
+
+// let value=Number(score);
+// console.log(value)
+// console.log(typeof value)
+
+// let value_1=Number(score_1);
+// console.log(value_1);// IT WILL GIVE NaN as o/p
+// console.log(typeof value_1);
+
+// let value_2=Number(score_2);
+// console.log(value_2);
+// console.log(typeof value_2);
+
+// let value_3=Number(score_3);
+// console.log(value_3);
+// console.log(typeof value_3);
+
+// let value_4=Number(score_4);
+// console.log(value_4);
+// console.log(typeof value_4);
+
+// let value_5=Number(score_5);
+// console.log(value_5);
+// console.log(typeof value_5);
+
+/*
+"33" => 33
+"33anc" => NaN
+true => 0 / false => 0
+"anc" => NaN
+undefined =>NaN
+null => 0   
+*/
+
+//Conversion to Boolean
+//  let num=null
+//  let boolnum= Boolean(num)
+ 
+//  console.log(boolnum)
+//  console.log(typeof boolnum)
+
+/*
+1=> true; 0=>false
+""=> false
+"Arjun"=>true
+undefined=>false
+null=>false
+*/
+
+//Conversion to string
+// let num=null
+
+// let string_num= String(num)
+// console.log(string_num)
+// console.log(typeof string_num)
+/*
+63=>63
+true=>true
+undefined=>i=undefined
+null=>null
+all are in string
+*/
