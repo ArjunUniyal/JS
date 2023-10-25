fc70d9f2e60afd9bf24710b30c2036d85d897f20
Arjun Uniyal <142481196+LordMajnubhai@users.noreply.github.com>
2023-10-25 19:45:17 +0000

    summary on datatypes

---
+++ 5_datatypes_summary.js
@@ -0,0 +1,33 @@
+// /*Primitive Type */
+// /*these are call by value */
+// let num=54 //number
+// let str="arjun" //string
+// let result=false //boolean
+// let temp=null //object
+// let unknown //undefined
+// let bignum=25n //bigint
+// let id=Symbol("123") //symbol
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof result);
+// console.log(typeof temp);
+// console.log(typeof unknown);
+// console.log(typeof bignum);
+// console.log(typeof id);
+// let id1=Symbol("123")
+// console.log(id==id1); // it will give false. it does not matter if two different variable have same symbol, they are not equal
+
+/* non primitve or reference datatypes */
+// array, objects, function
+
+// let arr=[1,"Arjun"]
+// let obj={
+//     name:"Arjun",
+//     age:22
+// }
+// let func= function(){
+//     console.log("hello world");
+// }
+// console.log(typeof arr); //object
+// console.log(typeof obj); //object
+// console.log(typeof func); //function object
