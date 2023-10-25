6fe8f3d41ac8eada6808d1e905b2d66bbd761f0e
Arjun Uniyal <142481196+LordMajnubhai@users.noreply.github.com>
2023-10-25 19:06:23 +0000

    operators

---
+++ basics/4_operations.js
@@ -0,0 +1,36 @@
+// let num=7
+// let negnum= -num
+// console.log(negnum);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(3/2);
+// console.log(2**3);
+// console.log(3%2);
+// str1="Arjun"
+// str2=" Uniyal"
+// str3=str1+str2
+// console.log(str3);
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log(1+"2"+3);
+// console.log(1+2+"3");
+
+// console.log(+true);
+// console.log(-true);
+// console.log(+false);
+// console.log(-false);
+
+// console.log(true); // this will give an error
+
+//prefix and postfix
+let counter=100
+const y=++counter
+console.log(counter)
+console.log(y)
+
+const z=counter++
+console.log(counter)
+console.log(z)
+
